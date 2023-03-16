f039771bb03cb5a4f09529bee51311243a326b8f
IngaLa <inga.las@outlook.com>
2023-03-16 20:45:36 +0200

    Deleted a line

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+
 Console.WriteLine("... Inga is here");
 Console.WriteLine("added new lines here");
